a68af60f115319ca7aa9e9b94a65dde36cc8b14e
Jack Morrison <jmorrison@mitre.org>
2012-01-23 18:08:49 -0500

    Adds configuration options and configure_file command.

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,19 +1,29 @@
 #include "say.hpp"
+#include "SayString.h"
 #include <iostream>
 #include <boost/lexical_cast.hpp>
 
 int main(int argc, char *argv[])
 {
+#ifdef SHOULD_REPEAT
     int numTimes = 1;
     if (argc > 1) {
         try {
             numTimes = boost::lexical_cast<int>(argv[1]);
         } catch (...) {}
     }
+#endif
 
+#ifdef SHOULD_REPEAT
     for (int i = 0; i < numTimes; ++i) {
-        mitre::sayHello(std::cout, "World!");
+#endif
+
+        mitre::sayHello(std::cout, "World " + STRING_TO_SAY);
         std::cout << std::endl;
+
+#ifdef SHOULD_REPEAT
     }
+#endif
+
     return 0;
 }
