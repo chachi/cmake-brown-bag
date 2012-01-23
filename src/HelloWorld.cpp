e8fa423184346b6d0239b241684c824fda66652d
Jack Morrison <jmorrison@mitre.org>
2012-01-23 16:59:20 -0500

    Adds Boost functionality.

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -1,8 +1,19 @@
 #include "say.hpp"
 #include <iostream>
+#include <boost/lexical_cast.hpp>
 
 int main(int argc, char *argv[])
 {
-    mitre::sayHello(std::cout, "World!");
+    int numTimes = 1;
+    if (argc > 1) {
+        try {
+            numTimes = boost::lexical_cast<int>(argv[1]);
+        } catch (...) {}
+    }
+
+    for (int i = 0; i < numTimes; ++i) {
+        mitre::sayHello(std::cout, "World!");
+        std::cout << std::endl;
+    }
     return 0;
 }
